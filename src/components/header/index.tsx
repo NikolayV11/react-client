88df088ebd4159a3ae858de047c3f776e2b4bb39
Николай <wasilev-n@mail.ru>
2024-03-31 22:27:19 +0500

    импортировал библиатеки(react-route, react-icon), добавил навигацию и несколько страниц

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { ThemeContext } from "../theme-provider"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react"
+import { FaRegMoon } from "react-icons/fa"
+import { LuSunMedium } from "react-icons/lu"
+
+export const Header = () => {
+  const { theme, toggleTheme } = React.useContext(ThemeContext)
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Мини блог</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="lg:flex text-3x1 cursor-pointer">
+          {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+        <NavbarItem></NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
